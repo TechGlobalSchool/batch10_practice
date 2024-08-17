bdb2fe4edf8ff3b4a5202e95cce287abb0b8e838
AlinaHegyi <alina.hegyi@hotmail.com>
2024-08-18 04:09:52 +1200

    latest change

--- alina.js
+++ alina.js
@@ -3,3 +3,5 @@
 console.log('We are learning GitHub');
 //change 3
 // Git is the main technology, GitHub is the platform to share it and collaborate using it.
+//Adding more changes
+console.log('This is a nice class');
