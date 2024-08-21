bfcf4b39c85583cda15ab1c37d0ad0a4e5dd236a
DataTechBorbieva <data.b@outlook.com>
2024-08-21 15:51:29 -0500

    added new changes to cause a conflict

--- data.js
+++ data.js
@@ -1,3 +1,8 @@
 // This is Data
 
 let array = new Array ['D', 'a', 't', 'a'];
+console.log(`Here is my new line of code 
+    that i have to push in order to create a conflict.
+    It was our task for Git Homework.
+    It helped me to refresh my knowledge.`);
+    
