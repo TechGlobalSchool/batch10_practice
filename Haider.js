4a071a11786db14854f4b061a49aee1e93d3f876
aybars <kirkpinaraybars@gmail.com>
2024-08-21 20:43:10 -0400

    123

--- Haider.js
+++ Haider.js
@@ -1,9 +1,11 @@
 // this is Haider
 
-function hi(){
-  console.log('Hi');
+function hi() {
+  console.log("Hi");
 }
 
 // This is TechGlobal adding for conflict
 
 // This is new line
+
+// this is new line aybars
